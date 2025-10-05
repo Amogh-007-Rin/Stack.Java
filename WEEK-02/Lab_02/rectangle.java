0b6df7281c002c2cc84a9584030a16eba11ef127
Amogh-007-Rin <amoghdath233@gmail.com>
2025-10-05 23:57:05 +0100

    Stage-03

--- WEEK-02/Lab_02/rectangle.java
+++ WEEK-02/Lab_02/rectangle.java
@@ -1,4 +1,30 @@
-public class rectangle {
-    
+import java.util.*;
+
+public class Rectangle {
+    Vector v1;
+    Vector v2;
+
+
+    public Rectangle(Vector v1, Vector v2){
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public double width(){
+        return this.v1.x - this.v2.x;
+    }
+
+    public double height(){
+        return this.v1.y - this.v2.y;
+    }
+
+    public double area(){
+        return this.width() * this.height();
+    }
+
+
+    public double center(){
+       
+    }
 }
 
