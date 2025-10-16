825a6e616f7aa6fdfee0b2b58b062e23fb73050f
Amogh-007-Rin <amoghdath233@gmail.com>
2025-10-16 17:20:54 +0100

    Stage-07

--- WEEK-03/src/QueryStringMain.java
+++ WEEK-03/src/QueryStringMain.java
@@ -1,12 +1,27 @@
+// Manual testing for quary string
+
 public class QueryStringMain {
     
-
-
-
-
-
-
     public static void main(String args[]){
         
+        QueryString data = new QueryString("name=Ironman&age=20&country=UK");
+
+        System.out.println("Testing the getParameter function");
+        System.out.println();
+        System.out.println(data.getParameter("name"));
+        System.out.println("Testing the hasParameter function");
+        System.out.println(data.hasParameter("country"));
+        System.out.println();
+        System.out.println("Testing the getParameterOffset function");
+        System.out.println(data.getParameterOffset("UK"));
+        System.err.println("Testing the decode function");
+        System.out.println();
+        System.out.println(data.decode("Amogh"));
+
+
+        
+
+
+
     }
 }
