f52d765350fa8fc2b3c5bbe96097e10992f2df52
Amogh-007-Rin <amoghdath233@gmail.com>
2025-10-16 14:33:08 +0100

    Stage-06

---
+++ WEEK-03/src/QueryStringMain.java
@@ -0,0 +1,12 @@
+public class QueryStringMain {
+    
+
+
+
+
+
+
+    public static void main(String args[]){
+        
+    }
+}
--- WEEK-03/src/VendingMachineMain.java
+++ WEEK-03/src/VendingMachineMain.java
@@ -1,43 +1,54 @@
 
 
-public class VendingMachineMain{
 
+public class VendingMachineMain{
+    
     public static void main(String args[]){
       
         VendingMachine v1 = new VendingMachine();
         // Return the initial balance
+        System.out.println("======================== Checking the initial balance ===========================");
+        System.out.println();
         System.out.println("The initial balance is : " + v1.getBalance());
         System.out.println();
         // Returns the initial Revenue
-        System.out.println("The revenue is : " + v1.getRevenue());
+        System.out.println("======================== Checking the initial Revenue ===========================");
+        System.out.println();
+        System.out.println("The initial revenue is : " + v1.getRevenue());
         System.out.println();
         // checking if the insert coin function effect of balance and revenue
-        System.out.println("initial balance before inserting a coin :" + v1.getBalance());
+
+        System.out.println("========================= Tests for insert coin function =========================");
+        System.out.println();
+        System.out.println("Initial balance before inserting a coin : " + v1.getBalance());
         System.out.println("Initial revenue before inserting a coin : " + v1.getRevenue());
-        // inserting the coin into vending machine
-        v1.insertCoin();
-        v1.insertCoin();
-        v1.insertCoin();
-        v1.insertCoin();
-        v1.insertCoin();
-        v1.insertCoin();
-        v1.insertCoin();
-        System.out.println(" Balance after inserting a coin :" + v1.getBalance());
-        System.out.println(" Revenue after inserting a coin : " + v1.getRevenue());
+        System.out.println();
+        
+        // inserting the coins into vending machine
+        int i;
+        for(i = 0;  i<10; i++ ){
+          v1.insertCoin();
+        }
+       
+        System.out.println(" Balance after inserting " + i + " Coins : " + v1.getBalance());
+        System.out.println(" Revenue after inserting " + i + " Coins : " + v1.getRevenue());
         System.out.println();
         System.out.println("Inserting a coin to the vending machine increases the balance and the revenue remains the same");
         System.out.println();
         // Refund request
-        System.out.println(" Refunding your balance amount ");
+        System.out.println("================================= Refunding your balance amount =====================================");
         System.out.println("Refund amount : " + v1.refund());
         System.out.println("Your current Balance is : "+ v1.getBalance());
         
         // Vending Candy Bar function checking
+        
         System.out.println(v1.vendCandyBar());
         v1.insertCoin();
         v1.insertCoin();
         v1.insertCoin();
         System.out.println(v1.vendCandyBar());
+
+        
         
 
     }
